ff694123aae64dc19ccc74017887301c8ffef685
Ruslan Usman <your-email@example.com>
2025-04-16 01:24:40 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTelegram, faGoogleWallet } from '@fortawesome/free-brands-svg-icons';
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+const App = () => {
+  const donationCards = [
+    {
+      id: 1,
+      image: '/images/official-donation.jpg',
+      title: 'Официальный донат',
+      link: 'https://store.herogame.com/lios',
+      icon: faCreditCard, // Иконка кредитной карты для официального доната
+    },
+    {
+      id: 2,
+      image: '/images/playerok-donation.jpg',
+      title: 'Донат через Playerok',
+      link: 'https://playerok.com/apps/last-island-of-survival/coupons',
+      icon: faGoogleWallet, // Иконка Google Wallet для Playerok доната
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <header>
+        <h1>Донат Last Island of Survival</h1>
+      </header>
+      <main className="cards-container">
+        {donationCards.map((card) => (
+          <div className="card" key={card.id}>
+            <img src={card.image} alt={card.title} />
+            <h3>{card.title}</h3>
+            <a
+              href={card.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="donate-button"
+            >
+              <FontAwesomeIcon icon={card.icon} /> Перейти
+            </a>
+          </div>
+        ))}
+      </main>
+      <footer>
+        <p>Эпоха выживания</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://web.telegram.org/a/#-1002050285269"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FontAwesomeIcon icon={faTelegram} className="telegram-icon" />
         </a>
-      </header>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
